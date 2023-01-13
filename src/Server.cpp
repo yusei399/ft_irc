aa92dda6737f19b1ac559fc8c2c4db3175a91001
yusei399 <yusei2021@keio.jp>
2023-01-13 15:47:18 +0900

    通信ポートアドレスの設定

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,6 +7,8 @@ Server::~Server(){}
 void Server::create_soket()
 {
 	int enable = 1;
+	struct sockaddr_in reader_addr;
+	
 	// ソケット作成、アドレスドメイン、ソケットタイプ、プロトコル
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_fd < 0)
@@ -27,5 +29,12 @@ void Server::create_soket()
 		std::cout << "ERROR Fcntl" << std::endl;
 		exit(1);
 	}
+
+	//通信、ポートアドレスの設定
+	bzero((char *) &reader_addr, sizeof(reader_addr));
+	reader_addr.sin_family = AF_INET;
+	reader_addr.sin_addr.s_addr = INADDR_ANY;
+	reader_addr.sin_port = htonl(port_);
+
 	
 }
