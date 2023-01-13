67513f602e02bb34760f82d03197ce93d8dfeade
yusei399 <yusei2021@keio.jp>
2023-01-13 15:14:15 +0900

    ソケット作成アドレス、ソケットタイプ、プロトコル

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,5 +6,11 @@ Server::~Server(){}
 
 void Server::create_soket()
 {
-	
+	// ソケット作成、アドレスドメイン、ソケットタイプ、プロトコル
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd < 0)
+	{
+		std::cout << "ERROR socket" << std::strerror(errno);
+		exit(1);
+	}
 }
