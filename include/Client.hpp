3698ddb2f34d5eb8ee2aa26a378dcb61a4a392f0
yusei399 <yusei2021@keio.jp>
2023-01-14 17:19:57 +0900

    client_hpp

---
+++ include/Client.hpp
@@ -0,0 +1,13 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+class Client
+{
+private:
+	/* data */
+public:
+	Client(/* args */);
+	~Client();
+};
+
+#endif
---
+++ include/User.hpp
@@ -0,0 +1,18 @@
+#ifndef USER_HPP
+#define USER_HPP
+
+#include "Server.hpp"
+
+class User
+{
+private:
+	std::string name;
+	std::string nick_name;
+public:
+	User();
+	~User();
+
+	void user_setup();
+};
+
+#endif
