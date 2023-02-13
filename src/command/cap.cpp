f766ccb834ef21cb3c43eb25e35340e674e1f59d
susui <skymattya@gmail.com>
2023-02-14 08:55:51 +0900

    a

---
+++ src/command/cap.cpp
@@ -0,0 +1,16 @@
+# include "../../include/Channel.hpp"
+# include "../../include/Client.hpp"
+# include "../../include/Command.hpp"
+# include "../../include/Server.hpp"
+
+
+void cap()
+{
+	// input check
+	if ( == "LS")
+	else if ( == "LIST")
+	else if ( == "REQ")
+	else if ( == "ACK")
+	else if ( == "END")
+	else if ( == "ERROR")
+}
