b7a7bdf808dc767afe3d72df9ce8d047c4339e6a
susui <13848950-sora_grayscale@users.noreply.gitlab.com>
2023-03-03 17:08:33 +0900

    channel is done

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -7,21 +7,23 @@
 #include <vector>
 
 class Channel
-	{
+{
 private:
-	std::vector<Channel> channels; // IRCサーバーが持つチャンネルのリスト
-	std::string name; // チャンネル名
-	std::vector<std::string> users;
-	// チャンネルに参加しているユーザーのリスト
-	// その他、チャンネルの設定やメッセージの履歴などを保存するための変数を定義することもできる
+    std::vector<Channel> channels; // IRCサーバーが持つチャンネルのリスト
+    std::string name; // チャンネル名
+    std::vector<std::string> users;
+    // チャンネルに参加しているユーザーのリスト
+    // その他、チャンネルの設定やメッセージの履歴などを保存するための変数を定義することもできる
 
 public:
-	Channel();
-	~Channel();
-	void createChannel(std::string channelName);
-	void joinChannel(std::string channelName, std::string username);
-	void leaveChannel(std::string channelName, std::string username);
-	void sendMessage(std::string channelName, std::string message);
+    Channel();
+    ~Channel();
+	
+    void createChannel(std::string channelName);
+    void joinChannel(std::string channelName, std::string username);
+    void leaveChannel(std::string channelName, std::string username);
+    void sendMessage(std::string channelName, std::string username, std::string message);
+;
 };
-
 #endif
+
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -4,65 +4,71 @@ Channel::Channel() {}
 Channel::~Channel() {}
 
 
-// チャンネルを作成する関数
-void Channel::createChannel(std::string channelName)
-{
-		Channel newChannel;
-		newChannel.name = channelName;
-		channels.push_back(newChannel);
-}
-
 // チャンネルに参加する関数
 void Channel::joinChannel(std::string channelName, std::string username)
 {
-	// チャンネルが存在しなければ新しく作成する
-	auto it =
-			std::find_if(channels.begin(), channels.end(), [&](Channel &channel)
-			{
-				return channel.name == channelName;
-			});
-	if (it == channels.end())
-	{
-		createChannel(channelName);
-		it = channels.end() - 1;
-	}
-	// ユーザーをチャンネルに追加する
-	it->users.push_back(username);
+    // チャンネルが存在しなければ新しく作成する
+    std::vector<Channel>::iterator it;
+    bool found = false;
+    for (it = channels.begin(); it != channels.end(); ++it) {
+        if (it->name == channelName) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        createChannel(channelName);
+        it = channels.end() - 1;
+    }
+    // ユーザーをチャンネルに追加する
+    it->users.push_back(username);
 }
 
 // チャンネルから離脱する関数
 void Channel::leaveChannel(std::string channelName, std::string username) {
-	// チャンネルが存在するかどうかを確認する
-	auto it =
-			std::find_if(channels.begin(), channels.end(), [&](Channel &channel) {
-				return channel.name == channelName;
-			});
-	if (it != channels.end())
-	{
-		// ユーザーをチャンネルから削除する
-		auto user_it = std::find(it->users.begin(), it->users.end(), username);
-		if (user_it != it->users.end())
-		{
-			it->users.erase(user_it);
-		}
-	}
+    // チャンネルが存在するかどうかを確認する
+    std::vector<Channel>::iterator it;
+    for (it = channels.begin(); it != channels.end(); ++it) {
+        if (it->name == channelName) {
+            // ユーザーをチャンネルから削除する
+            std::vector<std::string>::iterator user_it;
+            for (user_it = it->users.begin(); user_it != it->users.end(); ++user_it) {
+                if (*user_it == username) {
+                    it->users.erase(user_it);
+                    break;
+                }
+            }
+            break;
+        }
+    }
 }
 
 // チャンネルにメッセージを送信する関数
-void Channel::sendMessage(std::string channelName, std::string message)
-{
-	// チャンネルが存在するかどうかを確認する
-	auto it = std::find_if(channels.begin(), channels.end(), [&](Channel &channel) {
-				return channel.name == channelName;
-			});
-	if (it != channels.end())
-	{
-		// メッセージをチャンネルに送信する
-		// ここで、メッセージを履歴に追加するなどの処理を行うこともできる
-		for (auto user : it->users)
-		{
-			// ユーザーにメッセージを送信する処理を実装する
-		}
-	}
+
+void Channel::sendMessage(std::string channelName, std::string username, std::string message) {
+    std::vector<Channel>::iterator it;
+    bool found = false;
+    for (it = channels.begin(); it != channels.end(); ++it) {
+        if (it->name == channelName) {
+            found = true;
+            break;
+        }
+    }
+    if (found) {
+        std::cout << "[" << it->name << "] " << username << ": " << message << std::endl;
+        for (std::vector<std::string>::iterator user_it = it->users.begin(); user_it != it->users.end(); ++user_it) {
+            if (*user_it != username) {
+                std::cout << "[Server] sending message to " << *user_it << std::endl;
+                // send message to user
+            }
+        }
+    }
 }
 
+int main() {
+    Channel channel;
+    channel.joinChannel("general", "Alice");
+    channel.joinChannel("general", "Bob");
+    channel.sendMessage("general", "Alice", "Hello, Bob!");
+    return 0;
+}
