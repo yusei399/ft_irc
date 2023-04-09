95f8a43f6002de3ecc478861db2ca6e37e0d54bc
yusei399 <yusei2021@keio.jp>
2023-04-09 10:57:23 +0900

    userコマンド設定

--- include/Client.hpp
+++ include/Client.hpp
@@ -12,6 +12,9 @@ private:
 	std::string _command;
 	std::vector<std::string> _params;
 	bool _permission;
+	std::string _user;
+	std::string _real_name;
+	std::string _hostname;
 
 public:
 	Client(/* args */);
@@ -27,6 +30,9 @@ public:
 	std::string get_nick();
 	const std::vector<std::string> &get_params() const {return _params;}
 	void set_nick(const std::string &nick) {_nick = nick;}
+	void set_user(const std::string &user) {_user = user;}
+	void set_real_name(const std::string &real_name) {_real_name = real_name;}
+	void set_hostname(const std::string &hostname) {_hostname = hostname;}
 };
 
 void send_message(const std::string &message, int fd, int flag);
--- src/USER.cpp
+++ src/USER.cpp
@@ -11,11 +11,17 @@ void user(Client &client)
 		send_message("461 " + nick + " :Not enough parameters", fd, 0);
 	
 	std::string const &user = client.get_params()[0];
-	std::string const &mode = client.get_params()[1];
+	std::string const &host_name = client.get_params()[1];
 	std::string const &real_name = client.get_params()[2];
 
-	send_message("001 " + nick + " :Welcome to the Internet Relay Network " + nick, fd, 0);
+	if (client.get_params().size() == 4)
+	{
+		client.set_user(user);
+		client.set_hostname(host_name);
+		client.set_real_name(real_name);
+		send_message("001 " + nick + " :Welcome to the Internet Relay Network " + nick + "!", fd, 0);
+	}
 	
-	if (client.get_params().size() > 3)
+	if (client.get_params().size() > 4)
 		send_message("461 " + nick + " :Too many parameters", fd, 0);
 }
