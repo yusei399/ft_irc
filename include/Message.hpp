ebf73a968c3526606bbaacd90e1eeda69a809eb9
ktada42 <kaou.tad.147.258.369@gmail.com>
2023-05-21 19:10:20 +0900

    現状使ってないreplyをコメントアウトした

--- include/Message.hpp
+++ include/Message.hpp
@@ -51,29 +51,31 @@ const std::string server_name = "ircserv";
 #define RPL_NONE(msg) 									REPLY(server_name, "300", "*", msg)
 
 #define RPL_NICK_MSG(sender, command, parameters, trailing)	REPLY(server_name, command, sender.get_nick() + ((std::string)parameters == "" ? (std::string)"" : (" " + (std::string)parameters)), trailing)
-#define RPL_CHANNELMODEIS(sender, ch, mode) 			RPL_NICK_MSG(sender, "324", ch.get_name() + " " + (std::string)mode)
-#define RPL_NOTOPIC(sender, ch) 						RPL_NICK_MSG(sender, "331", "TOPIC " + ch.get_name(), "No topic set for "+ ch.get_name())
+//#define RPL_CHANNELMODEIS(sender, ch, mode) 			RPL_NICK_MSG(sender, "324", ch.get_name() + " " + (std::string)mode)
+//#define RPL_NOTOPIC(sender, ch) 						RPL_NICK_MSG(sender, "331", "TOPIC " + ch.get_name(), "No topic set for "+ ch.get_name())
 #define RPL_TOPIC(sender, ch, topic)    				RPL_NICK_MSG(sender, "332", ch.get_name(), topic)
-#define RPL_INVITING(sender, target, ch) 				RPL_NICK_MSG(sender, "341", target.get_nick()+ " " + ch.get_name())
-#define RPL_NAMREPLY(sender, ch, name_list) 			RPL_NICK_MSG(sender, "353", " = " + ch.get_name(),name_list)
-#define RPL_ENDOFNAMES(sender, ch) 						RPL_NICK_MSG(sender, "366", ch.get_name(),"End of /NAMES list")
+//#define RPL_INVITING(sender, target, ch) 				RPL_NICK_MSG(sender, "341", target.get_nick()+ " " + ch.get_name())
+//#define RPL_NAMREPLY(sender, ch, name_list) 			RPL_NICK_MSG(sender, "353", " = " + ch.get_name(),name_list)
+//#define RPL_ENDOFNAMES(sender, ch) 						RPL_NICK_MSG(sender, "366", ch.get_name(),"End of /NAMES list")
 //									+k, -kなど
 //#define RPL_CREATIONTIME(nick, channelName, nowTime) RPL_NICK_MSG(sender, "329", " #" + channelName + " " + nowTime)
 
 //ERR
 #define ERR_NOSUCHNICK(sender, target_nick) 					RPL_NICK_MSG(sender, "401", target_nick, "No such nick")
 #define ERR_NOSUCHCHANNEL(sender, ch_name)					RPL_NICK_MSG(sender, "403", ch_name, "No such channel")
-#define ERR_NOTJOINCHANNEL(sender, ch)					RPL_NICK_MSG(sender, "404", ch.get_name(), "Cannot send to channel (+n)")
-#define ERR_TOOMANYCHANNELS(sender, ch)					RPL_NICK_MSG(sender, "405", ch.get_name(), "Too many channel")
+//#define ERR_NOTJOINCHANNEL(sender, ch)					RPL_NICK_MSG(sender, "404", ch.get_name(), "Cannot send to channel (+n)")
+//#define ERR_TOOMANYCHANNELS(sender, ch)					RPL_NICK_MSG(sender, "405", ch.get_name(), "Too many channel")
 #define ERR_NORECIPIENT(sender, command) 				RPL_NICK_MSG(sender, "411", "", "No recipient given (" + command + ")")
 #define ERR_NOTEXTTOSEND(sender)						RPL_NICK_MSG(sender, "412", "", "No text to send")
 //todo unknown
 #define ERR_UNKNOWNCOMMAND(sender, cmd_name)			RPL_NICK_MSG(sender, "421", cmd_name, "Unknown command")
-#define ERR_NONICKNAMEGIVEN 							REPLY(":"+server_name, "431", "", "No nickname given")
+//#define ERR_NONICKNAMEGIVEN 							REPLY(":"+server_name, "431", "", "No nickname given")
 #define ERR_ERRONEUSNICKNAME(sender)					RPL_NICK_MSG(sender, "432", "", "Error one use nickname")
 #define ERR_NICKNAMEINUSE(sender, new_nick)				RPL_NICK_MSG(sender, "433", new_nick, "Nickname is already in use")
+#define ERR_USERNOTINCHANNEL(sender, target, ch)		RPL_NICK_MSG(sender, "441", target.get_nick() + ch.get_name(), "They aren't on that channel")
 #define ERR_NOTONCHANNEL(sender, ch)					RPL_NICK_MSG(sender, "442", ch.get_name(), "You're not on that channel")
 #define ERR_USERONCHANNEL(sender, target, ch) 			RPL_NICK_MSG(sender, "443", target.get_nick()+" "+ch.get_name(), "User is already on that channel")
+#define ERR_NOTREGISTERED(sender)						RPL_NICK_MSG(sender, "451", "", "You have not registered")
 #define ERR_BADCHANNELKEY(sender, ch) 					RPL_NICK_MSG(sender, "457", ch.get_name(), "Cannot join channel (+k) - bad key")
 #define ERR_NEEDMOREPARAMS(sender, cmd_name)			RPL_NICK_MSG(sender, "461", cmd_name, "Not enough parameters")
 #define ERR_ALREADYREGISTRED(sender) 					RPL_NICK_MSG(sender, "462", "", "You may not reregister")
@@ -81,15 +83,13 @@ const std::string server_name = "ircserv";
 #define ERR_CHANNELISFULL(sender, ch) 					RPL_NICK_MSG(sender, "471", ch.get_name(), "Cannot join channel (+l)")
 #define ERR_NOCHANMODES(sender, mode) 					RPL_NICK_MSG(sender, "472", mode, "is an unknown mode character to me")
 #define ERR_INVITEONLYCHAN(sender, ch)					RPL_NICK_MSG(sender, "473", ch.get_name(), + "Cannot join channel (+i)")
-#define ERR_NOPRIVILEGES(sender)						RPL_NICK_MSG(sender, "481", "", "Permission Denied- You're not an " + sever_name +" operator")
+#define ERR_BADCHANNELKEY(sender, ch)						RPL_NICK_MSG(sender, "475", ch.get_name(), "Cannot join channel (+k)")
+//#define ERR_NOPRIVILEGES(sender)						RPL_NICK_MSG(sender, "481", "", "Permission Denied- You're not an " + sever_name +" operator")
 #define ERR_CHANOPRIVSNEEDED(ch)						REPLY(":"+server_name, "482", ch.get_name(), "You're not a channel operator")
 
-#define ERR_USERNOTINCHANNEL(sender, target, ch)			RPL_NICK_MSG(sender, "441", target.get_nick() + ch.get_name(), "They aren't on that channel")
-#define ERR_NOTREGISTERED(sender)							RPL_NICK_MSG(sender, "451", "", "You have not registered")
-#define ERR_BADCHANNELKEY(sender, ch)						RPL_NICK_MSG(sender, "475", ch.get_name(), "Cannot join channel (+k)")
 
 void send_msg(const Client &reciever, const std::string &message);
-//void send_numeric_msg(const Client &reciever, int err_code, const std::string &message);
+
 void send_welcome_msgs(const Client &reciever);
 void send_prl_none(const Client &reciever, const std::string &msg);
 #endif // MESSAGE_HPP
