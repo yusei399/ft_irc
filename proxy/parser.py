2a6f3f0b5f8a227b3f7ea8f896b1b567be4837bc
yusei399 <yusei2021@keio.jp>
2023-05-26 00:22:33 +0900

    pythonコード削除

--- proxy/parser.py
+++
@@ -1,6 +0,0 @@
-def parse(data, port, origin):
-    print ("[" + origin + ":" + str(port) + "]" + "\n" + str(data))
-        #print ":".join("{:02x}".format(ord(c)) for c in data)
-
-#il y a un lf ( + un cr) en fin de data ducoup c'est pour ca que ca print sur 2 lignes, faites un bail du genre pour print sur 1:
-#print ("[" + origin + ":" + str(port) + "]" + data, end='') je c meme pas si ca marche mais c'est un bail du genre pour sur
